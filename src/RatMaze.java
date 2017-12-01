c93e2a2ea3ceaabb0327e11b130b499eeae4181c
Ankit Kumar <ankit.kumar071460@gmail.com>
2017-12-01 23:53:55 +0530

    FIX: reverting from visited path

--- src/RatMaze.java
+++ src/RatMaze.java
@@ -4,72 +4,86 @@ public class RatMaze {
      maze of a rat
      1 represent path
      0 represent blocked cell
-     Destination is [3][3](last corner)
+     Destination is [5][5](last corner)
      */
     static int[][] maze = {
-            {1, 0, 0, 0},
-            {1, 1, 1, 1},
-            {0, 0, 0, 1},
-            {1, 1, 1, 1}};
+            {1, 1, 1, 0, 0},
+            {0, 0, 1, 0, 0},
+            {1, 1, 1, 1, 0},
+            {1, 1, 0, 1, 1},
+            {1, 1, 1, 1, 1}};
 
     static int[][] path;
+    static int[][] visited;
+    static int DIMENSION = 5;
 
     public static void main(String[] args) {
-        path = new int[4][4];
-        for(int i=0;i<4;i++){
-            for(int j=0;j<4;j++){
+
+        path = new int[DIMENSION][DIMENSION];
+        visited = new int[DIMENSION][DIMENSION];
+
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
                 path[i][j] = 0;
+                visited[i][j] = 0;
             }
         }
         //initial position of rat
         path[0][0] = 1;
-        String msg = move(0,0)?"Destination Reached":"No Path found!";
+        visited[0][0] = 1;
+        String msg = move(0, 0) ? "Destination Reached" : "No Path found!";
         p(msg);
         showPath();
 
     }
 
     private static boolean move(int x, int y) {
+
         //final destination
-        if(x==3 && y==3) {
-          //  p("Destination");
+        if (x == DIMENSION - 1 && y == DIMENSION - 1) {
+            p("Destination");
             return true;
         }
+
         // move forward
-        if(canMoveTo(x+1,y)){
-        //    p("Moving to x="+(x+1)+" y="+y);
-            int is =  move(x+1,y)?1:0;
-            path[x+1][y] = is;
-            if(is==1) {
+        if (canMoveTo(x + 1, y)) {
+            //p("Moving to x=" + (x + 1) + " y=" + y);
+            visited[x + 1][y] = 1;
+            int is = move(x + 1, y) ? 1 : 0;
+            path[x + 1][y] = is;
+            if (is == 1) {
                 return true;
             }
         }
+
         // move down
-        if(canMoveTo(x,y+1)){
-            //p("Moving to x="+(x)+" y="+(y+1));
-            int is =  move(x,y+1)?1:0;
-            path[x][y+1] = is;
-            if(is==1)
+        if (canMoveTo(x, y + 1)) {
+            //p("Moving to x=" + (x) + " y=" + (y + 1));
+            visited[x][y + 1] = 1;
+            int is = move(x, y + 1) ? 1 : 0;
+            path[x][y + 1] = is;
+            if (is == 1)
                 return true;
         }
-        // move left
-        if(canMoveTo(x-1,y)){
-            //p("Moving to x="+(x-1)+" y="+y);
-            int is = move(x-1,y) ? 1:0;
-            path[x-1][y] = is;
-            if(is==1)
-                return true;
 
+        // move left
+        if (canMoveTo(x - 1, y)) {
+            //p("Moving to x=" + (x - 1) + " y=" + y);
+            visited[x - 1][y] = 1;
+            int is = move(x - 1, y) ? 1 : 0;
+            path[x - 1][y] = is;
+            if (is == 1)
+                return true;
         }
 
         return false;
 
     }
 
-    private static void showPath(){
-        for(int i = 0;i<4;i++){
-            for(int j = 0;j<4;j++){
-                System.out.print("["+path[i][j]+"]");
+    private static void showPath() {
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
+                System.out.print("[" + path[i][j] + "]");
             }
             System.out.println("");
         }
@@ -77,17 +91,14 @@ public class RatMaze {
 
     private static boolean canMoveTo(int x, int y) {
 
-        if(x>=0 && y>=0 && x<=3 && y<=3){
-            return maze[x][y] != 0;
-        }else{
+        if (x >= 0 && y >= 0 && x <= DIMENSION - 1 && y <= DIMENSION - 1) {
+            return (maze[x][y] != 0) && visited[x][y] == 0;
+        } else {
             return false;
         }
-
-
-
     }
-
-    private static void p(String s){
+    
+    private static void p(String s) {
         System.out.println(s);
     }
 

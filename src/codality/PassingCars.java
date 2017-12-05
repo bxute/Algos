42b2298aa73ed87b17b5dabc535d5a9e9673af31
Ankit Kumar <ankit.kumar071460@gmail.com>
2017-12-05 19:05:39 +0530

    ADD: added codality problem

--- src/codality/PassingCars.java
+++ src/codality/PassingCars.java
@@ -1,15 +1,15 @@
 package codality;
 
-/*
-* Problem Statement at : https://codility.com/programmers/lessons/5-prefix_sums/passing_cars
-* */
+        /*
+         * Problem Statement at : https://codility.com/programmers/lessons/5-prefix_sums/passing_cars
+         * */
 
 public class PassingCars {
 
     public static void main(String[] args) {
 
         int[] A = {0, 1, 0, 1, 1};
-        p("Carss Passing : "+getPassingCars(A));
+        p("Carss Passing : " + getCars(A));
 
     }
 
@@ -27,7 +27,7 @@ public class PassingCars {
                     inc[i] = inc[i + 1];
                     sum += inc[i];
 
-                    if(sum>1000000000){
+                    if (sum > 1000000000) {
                         return -1;
                     }
 
@@ -41,9 +41,27 @@ public class PassingCars {
 
         }
 
-        sum+=A[0]==0?inc[0]:0;
+        sum += A[0] == 0 ? inc[0] : 0;
 
         return sum;
+    }
+
+    // Another short solution
+    private static int getCars(int[] A) {
+
+        int east_count = 0;
+        int cars = 0;
+
+        for (int i = A.length - 1; i >= 0; i--) {
+            if (A[i] == 1) {
+                east_count++;
+            } else {
+                cars += east_count;
+                if(cars>1000000000)
+                    return -1;
+            }
+        }
+        return cars;
     }
 
     private static void p(String s) {
